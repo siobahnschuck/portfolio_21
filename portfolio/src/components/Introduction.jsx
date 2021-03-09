ec6df8cec7c3771987c9d508fc3bfad746f4ac5e
siobahnschuck <sschuc@saic.edu>
2021-03-09 09:24:55 -0600

    added font

--- portfolio/src/components/Introduction.jsx
+++ portfolio/src/components/Introduction.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Header from './Header'
+import earth from '../imgs/earthBackground.jpeg'
 
 const Introduction = () => {
 
@@ -8,9 +9,15 @@ const Introduction = () => {
     <div> 
       <Header/>
       <div>
-      <p> 
-        Siobahn Schuck is a Fullstack Developer and Video Artist living and working in Chicago, Illinois. 
+      <p className="intro"> 
+        Siobahn Schuck is a Fullstack Developer and Video Artist, 
+        <br/>
+        focused on creating tools for other creatives
+        <br/> that both spark joy and foster community.
+        <br/>
+        <button>Enter</button>
       </p>
+      <img className="earth" src={earth} alt="earth" /> 
       </div>
     </div>
   )
