3bd4519f673c9724c9bd6033d21b4456e38e4c40
siobahnschuck <sschuc@saic.edu>
2021-04-26 13:09:38 -0500

    adjust nav bar:

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -7,19 +7,10 @@ const About = () => {
   return (
     <div id="about" className="about-container">
       <section className="about-main">
-        {/* <h1 className="title">ABOUT ME</h1> */}
         <p className="about-txt">
-          I'm a Chicago based software developer and Video Artist who is passionate about eloquent programs and intuitive interfaces. With a diverse background in New Media Art and past experience working as a museum curator, I bring a variety of approaches to solving problems creatively and efficiently with code.
-      <br /> I thrive in team structured enviornments where collaboration is the key to success. I am detail oriented and believe even the smallest components have a big picture impact. Overall my interests lie at the intersection of art, humans, and the world wide web.
+          Hello! I am a Chicago based Software Developer and Video Artist who is passionate about eloquent programs and intuitive interfaces. With a diverse background in New Media Art and past experience working as a museum curator, I bring a variety of approaches to solving problems creatively and efficiently with code.
+      <br /> I thrive in team structured environments where collaboration is the key to success. I am detail oriented and believe even the smallest components have a big picture impact. Overall my interests lie at the intersection of art, humans, and the world wide web.
       </p>
-      </section>
-
-      <section className="skills">
-        <h1 className="title">SKILLS</h1>
-        <p>An evergrowing list</p>
-      </section>
-      <section className="element">
-        <h3>Another quote or image of something</h3>
       </section>
     </div>
   )
---
+++ portfolio/src/components/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>
+      <p> TECHNICAL TOOLKIT</p>
+    </div>
+  )
+}
+
+export default Skills
